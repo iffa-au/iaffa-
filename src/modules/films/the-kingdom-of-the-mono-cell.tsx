aa4038b4c608c434ba4791c542dfb9a9226690e9
Navneeth Prasanth <navneeth2412@gmail.com>
2026-03-09 18:39:59 +1100

    bug fix

--- src/modules/films/the-kingdom-of-the-mono-cell.tsx
+++ src/modules/films/the-kingdom-of-the-mono-cell.tsx
@@ -10,7 +10,7 @@ export default function TheKingdomOfTheMonoCellFilmPage() {
         <div className="max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 py-12">
           <div className="relative overflow-hidden rounded-lg shadow-2xl max-w-2xl mx-auto">
             <Image
-              src="/images/Films/The_Kingdom_of_the_mono-cell.webp"
+              src="/images/Films/The_kingdom_of_the_mono-cell.webp"
               alt="The Kingdom of the Mono Cell - Film Poster"
               width={800}
               height={1200}
